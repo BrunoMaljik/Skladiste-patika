91266e22d9859d16e609c2e8b632f279e6a5ef1e
BrunoMaljik <133969981+BrunoMaljik@users.noreply.github.com>
2023-05-18 23:20:11 +0200

    Add files via upload

---
+++ funkcije.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Header.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+int vracaBrojPatika() {
+
+	int brojPatika;
+	printf("Unesite kolicinu patika: ");
+	scanf("%d", &brojPatika);
+	return brojPatika;
+}
+
