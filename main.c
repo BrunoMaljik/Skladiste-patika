244dd15578857f19c4a6d6f86fc3931cfff10031
BrunoMaljik <133969981+BrunoMaljik@users.noreply.github.com>
2023-05-25 13:33:16 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,48 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <string.h>
+#include "functions.h"
+#include<stdio.h>
+#include<stdlib.h>
+
 int main(void) {
-	int option;
-	while (1) {
-		system("cls");
-		printf("\nUnesite broj opcije koju zelite: \n\n");
-		printf("(1)Dodavanje patika.\n");
-		printf("(2)Pretraga odredene patike.\n");
-		printf("(3)Ispis najjeftinih patika.\n");
-		printf("(4)Ispis najskupljih patika.\n");
-		printf("(5)Brisanje patika.\n");
-		printf("(6)Sortiranje patika.\n");
-		printf("(7)Izlazak iz programa.\n\n");
-		scanf("%d", &opcija);
-		switch (opcija) {
-		case 1:
-			unosPodataka();
-			break;
-		case 2:
-			pretragaPatika();
-			break;
-		case 3:
-			pretragaMin();
-			break;
-		case 4:
-			pretragaMax();
-			break;
+	inicijalizacija("patike.bin");
 
-		case 5:
-			brisanjePatika();
-			break;
+	int opcija = 1;
 
-		case 6:
-			sortiranje();
-			break;
-
-		case 7:
-			izlaz();
-			break;
-		}
+	while (opcija) {
+		opcija = izbornik("patike.bin");
 	}
+
+	printf("Kraj programa");
 
 	return 0;
 }
